f462f57f179d08163cf15eaab4e74205c49ef7cc
Rob W <robpwatkins@gmail.com>
2020-03-04 10:14:18 -0600

    "ho boy"

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let operation = null
 
 // this function takes in the number you type in the input field and saves it to the "firstNum" variable
 const saveFirstNumber = (num) => {
+  console.log(num);
   firstNum = parseInt(num)  
 }
 
@@ -33,13 +34,18 @@ const multiply = (numA, numB) => {
   // * to get a product then return it
   // Open up the inspector tool in Chrome and select the Console tab to see what this functions is "logging out" to the console.
   console.log(numA, numB)
+  return numA * numB;
 }
 
-const divide = null
-// / to get a quotient,
+const divide = (numA, numB) => {
+  // / to get a quotient,
+  return numA / numB;
+}
 
-const modulus = null
-// and % to get a remainder.
+const modulus = (numA, numB) => {
+  // and % to get a remainder.
+  return numA % numB;
+}
 
 // This function changes the "operation" variable to be equal to the "id" of the button we choose on the web page.
 const changeOperation = (chosenOperation) => {
@@ -64,11 +70,11 @@ const equals = () => {
     break;
     case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
     break;
-    case "multiplication": multiply(firstNum, secondNum) 
+    case "multiplication": putResultInElement(multiply(firstNum, secondNum)) 
     break;
-    case "division": console.log(divide(firstNum, secondNum)) 
+    case "division": putResultInElement(divide(firstNum, secondNum)) 
     break;
-    case "modulus": console.log(modulus(firstNum, secondNum)) 
+    case "modulus": putResultInElement(modulus(firstNum, secondNum)) 
     break;
     default: "Choose an operation"
   }
